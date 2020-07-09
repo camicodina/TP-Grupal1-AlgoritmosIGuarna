353e1130b5dae5de03cff932191fe97e31c2ddb0
Camila-Codina <Camila.Codina@ibm.com>
2020-07-08 23:10:21 -0300

    archivos principales

---
+++ main.py
@@ -0,0 +1,12 @@
+import panel_de_funciones,consulta_de_funciones,analizador,arbol_de_invocacion,info_desarrollador
+
+def cargar_archivo():
+    return archivo
+
+def analizar_archivo():
+    return fuente_unico, comentarios
+
+def main():
+    return main
+
+main()
