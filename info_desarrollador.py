7fd91b782ff5070ef4e75ad400a753bad0a7c73a
Camila-Codina <Camila.Codina@ibm.com>
2020-07-13 21:28:09 -0300

    template info_desarrollador

---
+++ info_desarrollador.py
@@ -0,0 +1,18 @@
+
+# Datos sobre la participación de cada uno de los integrantes por funcion 
+def autores(comentarios):
+    return 
+
+def porcentaje(autores, fuente_unico):
+    return
+
+
+################### Bloque Principal ###################
+
+
+comentarios = open("comentarios.csv")
+fuente_unico = open("fuente_unico.csv")
+participacion = open("participacion.txt","w")
+procesar_archivos(desarrolladores, participacion)
+comentarios.close()
+participacion.close()
