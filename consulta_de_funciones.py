072f2283d3b898db1884b362376322cc2c1e98bc
Camila-Codina <Camila.Codina@ibm.com>
2020-07-17 23:35:24 -0300

    consulta_de_funciones sin verificar

--- consulta_de_funciones.py
+++ consulta_de_funciones.py
@@ -1,55 +1,63 @@
-# Ver nombres de las funciones uno al lado del otro encolumnados
+def listar_funciones():
+    """[Autor: Camila Codina]
+       [Ayuda: Toma valores de comentarios.csv]
+    """
+    with open('comentarios.csv','r') as comentarios:
+        funciones = {}
+        for linea in comentarios:
+            nombre_funcion, autor, ayuda_uso, otro= linea.rstrip("\n").split(",")
+            funciones[nombre_funcion] = [autor, ayuda_uso, otro]
 
-def tabla_nombre_funciones(comentarios):
-    tabla = {}
-    #diccionario con el nombre de la funcion y un
-    return tabla
+    return funciones
 
-# Mensaje: "Funcion:" esperando uno de los nombres listados
-# Seguido por 
-# ?: descripción asociada al uso de la función en comillas triples
-# (+ parámetros formales que espera, el módulo al que pertenece, y el autor)
-# #: Todo lo relativo a la funcion
-# Continua preguntando hasta que enter
-
-
-def signo_pregunta(funcion):
-    return 
-
-def todo(funcion):
-    return
-
-# Si usuario ingresa "?todo": ? pero para toda funcion
-
-def pregunta_todo(funcion):
-    return
-
-# "#todo"
-
-def todo_todo(funcion):
+def crear_tabla(funciones):
+    """[Autor: Camila Codina]
+       [Ayuda: Genera una tabla con los nombres de las funciones que se podrían analizar]
+    """
+    Tabla = """\
+        +---------------------------------------------------------------------+
+        |    +++++++++++++++++ FUNCIONES DEL PROGRAMA ++++++++++++++++++++    |
+        |---------------------------------------------------------------------|
+            {}
+        +---------------------------------------------------------------------+\
+        """
+        # for funcion in funciones:
+        #     Tabla = Tabla.format("\n".join("{1}{0}{1} {1}{0}{1} {1}{0}{1} {1}{0}{1}").format(funcion, "|"))
+    print(Tabla)
     return
 
 
-def respuesta_input(funcion):
-    funcion_input = input("Función: ")
+def respuesta_input(funciones):
+    """[Autor: Camila Codina]
+       [Ayuda: Input del usuario y respuesta del programa]
+    """
+    funcion_input = input(str("Función: "))
+    pedido = funcion_input.split(" ")
     while funcion_input != "":
-        if "?todo":
-            pregunta_todo(funcion)
-        elif "#todo":
-            todo_todo(funcion)
-        elif "?":
-            signo_pregunta(funcion)
-        elif "#":
-            todo(funcion)
-        elif "imprimir ?todo":
-            print(ayuda)
+        if pedido[0] in funciones:
+            if pedido[1] == "?":
+                print(funciones[pedido[0]][1])
+            elif pedido[1] == "#":
+                print(funciones[pedido[0]])
+            else:
+                print("Carácter inválido. Intente nuevamente.")
+        elif funcion_input == "?todo":
+            for i in funciones:
+                print(i, "\n" ,funciones[i][1])
+        elif funcion_input == "#todo":
+            for i in funciones:
+                print(i, "\n" ,funciones[i])
+        elif funcion_input == "imprimir ?todo":
+            print(ayuda(ayuda))
         else:
             print("Función inexistente y/o carácter inválido. Intente nuevamente.")
-        funcion_input = input("Función: ")
-          
+        funcion_input = input(str("Función: "))
+        pedido = funcion_input.split(" ")
     return
 
+def ayuda(ayuda):
 
+    return
 
 
 
@@ -57,9 +65,11 @@ def respuesta_input(funcion):
 ################### Bloque Principal ###################
 
 
-comentarios = open("comentarios.csv")
+# comentarios = open("comentarios.csv")
 ayuda = open("ayuda_funciones.txt")
-respuesta_input(comentarios)
-comentarios.close()
+funciones = listar_funciones()
+respuesta_input(funciones)
+# respuesta_input(comentarios)
+# comentarios.close()
 ayuda.close()
 
