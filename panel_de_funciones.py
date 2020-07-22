bb8150b5d873c6aee1a9a6b659ed224ab3a5bf57
Dany707 <67444608+Dany707@users.noreply.github.com>
2020-07-22 20:08:27 -0300

    Update panel_de_funciones.py

--- panel_de_funciones.py
+++ panel_de_funciones.py
@@ -6,7 +6,7 @@ def diccionario_de_funciones(fuente_unico):
 
     diccionario_funciones={}
     for linea in fuente_unico:
-        diccionario_funciones[linea[0]]={'Módulo':'', 'Parámetros': 0, 'Líneas':0, 'Invocaciones':[0], 'Returns':0, 'If': 0 , 'For': 0, 'While':0, 'Break':0, 'Exit':0, 'Coment':0, 'Ayuda':'','Autor':''}
+        diccionario_funciones[linea[0]]={'Funcion.Módulo':'', 'Parámetros': 0, 'Líneas':0, 'Invocaciones':0, 'Returns':0, 'If': 0 , 'For': 0, 'While':0, 'Break':0, 'Exit':0, 'Coment':0, 'Ayuda':'','Autor':''}
     
     return diccionario_funciones
 
@@ -16,6 +16,7 @@ def funcion_codigo(lista):
        [Ayuda: Dada una línea, analiza el código perteneciente a la función que se encuentra en ella.
        Es decir, la cantidad de 'returns', 'if', 'for','while', 'breaks', 'exit']
     """
+    
     c_returns=0 
     c_if=0
     c_for=0
@@ -50,13 +51,13 @@ def invocacion_a_funcion(funcion,lista):
        [Ayuda: Dada una función 'x' del programa, se analiza si ésta invoca a otra función 'y']
     """
 
-    invocacion_por_funcion=[]
+    invocacion_por_funcion=0
     
     k=0
 
     for i in range(3,len(lista)):
         
-        if funcion in lista[i]:
+            if funcion in lista[i]:
             #Cuento la cantidad de veces que la función 'x' es invocada por la función 'y'      
              k+=1
     
@@ -64,10 +65,31 @@ def invocacion_a_funcion(funcion,lista):
     # donde la primer componente es el nombre de la función 'y' y la segunda componente las veces que las veces que fue invocada la función 'x' 
     if k!=0: 
 
-        invocacion_por_funcion=[lista[0],k]
+        invocacion_por_funcion=k
 
     return invocacion_por_funcion
 
+
+def cantidad_parametros_codigo(lista):
+    """[Autor: Daniela Bolivar]
+       [Ayuda: Esta función se utilizará para contar los parámetros de una función por eso es que se analiza
+       a partir de la componente 1 de la lista]
+    """
+    n=0
+    T=True
+    k=1
+    while T and k< len(lista):
+        if ')' in lista[k]:
+            T=False
+            
+        n+=1
+        if '()' in lista[k]:
+            T=False
+            n=0
+        k+=1
+    #Se le suma 1 a la resta porque la tercera componente es el módulo
+    k=len(lista)-(k+1)
+    return n,k            
 
 def analisis_codigo_funciones(fuente_unico):
     """[Autor: Daniela Bolivar]
@@ -81,25 +103,25 @@ def analisis_codigo_funciones(fuente_unico):
     #Recorro línea por línea el csv
     for linea in fuente_unico:
         #transfomo la línea a analizar en una lista
-        lista=linea.strip().split(',') 
+        lista=linea.rstrip('\n').split(',') 
 
         #Analizo si hay funciones invocadas por la función de la línea
         for funcion in diccionario_funciones: 
 
-            if funcion != lista[0]: # Descarto qu esté analizando la línea donde se define la función
+            if funcion != lista[0]: # Descarto que esté analizando la línea donde se define la función
 
                 invocacion_a_funcion(funcion,lista)
 
-                diccionario_funciones[funcion]['Invocaciones'][0]+=invocacion_a_funcion(funcion,lista)[1]
-                diccionario_funciones[funcion]['Invocaciones'].append(invocacion_a_funcion(funcion,lista))
+                diccionario_funciones[funcion]['Invocaciones']+=invocacion_a_funcion(funcion,lista)
+                
 
         
         funcion_codigo(lista) #Analizo el resto del código de la línea
 
         #Completo el diccionario
-        diccionario_funciones[lista[0]]['Módulo']=lista[2]
-        diccionario_funciones[lista[0]]['Parámetros']=len(lista[1])
-        diccionario_funciones[lista[0]]['Líneas']=len(lista)-3
+        diccionario_funciones[lista[0]]['Funcion.Módulo']=str(lista[0])+'.'+str(lista[2])
+        diccionario_funciones[lista[0]]['Parámetros']=cantidad_parametros_codigo(lista)[0]
+        diccionario_funciones[lista[0]]['Líneas']=cantidad_parametros_codigo(lista)[1]
         diccionario_funciones[lista[0]]['Returns']=funcion_codigo(lista)[0]
         diccionario_funciones[lista[0]]['If']=funcion_codigo(lista)[1]
         diccionario_funciones[lista[0]]['For']=funcion_codigo(lista)[2]
@@ -119,8 +141,11 @@ def funcion_comentarios(linea):
     lista=linea.strip().split(',')
 
     comentarios=len(lista)-3
-        
-    autor= lista[1]
+
+    if lista[1]==' ':
+        autor='Anónimo'
+    else:     
+        autor= lista[1]
 
     ayuda= 'Si'
     if lista[2]=='':
@@ -142,7 +167,7 @@ def analisis_comentarios_funciones(diccionario_funciones,comentarios):
         diccionario_funciones[lista[0]]['Coment']=funcion_comentarios(linea)[0]
         diccionario_funciones[lista[0]]['Ayuda']=funcion_comentarios(linea)[1]
         diccionario_funciones[lista[0]]['Autor']=funcion_comentarios(linea)[2]
-        
+
     return diccionario_funciones
 
 
@@ -161,7 +186,6 @@ def longitud_columna(palabra,clave,diccionario):
 
     return n+2
 
-
 def longitud_caracteres(lista):
     """[Autor: Daniela Bolivar]
        [Ayuda: Danda una lista, obtengo la longitud de cada elemento de esta y le sumo dos, estos resultados son
@@ -170,7 +194,7 @@ def longitud_caracteres(lista):
     longitud=[]
 
     for i in range(0,len(lista)):
-        longitud.append(len(lista[i])+2)
+        longitud.append(len(lista[i])+1)
     
     return longitud
 
@@ -202,14 +226,14 @@ def panel_de_funciones(archivo, fuente_unico, comentarios):
 
 
     #En el archivo csv escribo el título de las columnas y a la vez los imprimo por pantalla los dejo centrados
-    print('{:^n}{:^longitud[0]}{:^longitud[1]}{:^longitud[2]}{:^longitud[3]}{:^longitud[4]}{:^longitud[5]}{:^longitud[6]}{:^longitud[7]}{:^longitud[8]}{:^longitud[9]}{:^longitud[10]}{:^m}'.format('FUNCION', 'Parámetros', 'Líneas', 'Invocaciones', 'Returns', 'If/Elif', 'For', 'While', 'Break', 'Exit', 'Coment', 'Ayuda', 'Autor'))
+    print('FUNCION',' '*(n-len('FUNCION')),'|Líneas ', '|Invocaciones ', '|Returns ', '|If/Elif ', '|For ', '|While ', '|Break ', '|Exit ', '|Coment ', '|Ayuda ', '|Autor ',' '*(n-len('FUNCION')),'|')
     
     archivo.write('{},{},{},{},{},{},{},{},{},{},{},{},{}\n'.format('FUNCION', 'Parámetros', 'Líneas', 'Invocaciones', 'Returns', 'If/Elif', 'For', 'While', 'Break', 'Exit', 'Coment', 'Ayuda', 'Autor'))
 
     #Escribo el archivo csv, donde cada línea corresponde al análisis de una función y a la vez los imprimo el análisis por pantalla
     for funcion in diccionario_funciones:
 
-        print('{:^n}{:^longitud[0]}{:^longitud[1]}{:^longitud[2]}{:^longitud[3]}{:^longitud[4]}{:^longitud[5]}{:^longitud[6]}{:^longitud[7]}{:^longitud[8]}{:^longitud[9]}{:^longitud[10]}{:^m}'.format( diccionario_funciones[funcion]['Funcion.Modulo'] , diccionario_funciones[funcion]['Parámetros'], diccionario_funciones[funcion]['Líneas'], diccionario_funciones[funcion]['Invocaciones'][0], diccionario_funciones[funcion]['Returns'], diccionario_funciones[funcion]['If'], diccionario_funciones[funcion]['For'], diccionario_funciones[funcion]['While'], diccionario_funciones[funcion]['Break'], diccionario_funciones[funcion]['Exit'], diccionario_funciones[funcion]['Coment'] , diccionario_funciones[funcion]['Ayuda'], diccionario_funciones[funcion]['Autor'] ))
+        print(diccionario_funciones[funcion]['Funcion.Modulo'], ' '*(n-len(diccionario_funciones[funcion]['Funcion.Modulo'])),'|', diccionario_funciones[funcion]['Parámetros'],' '*(longitud[0]-len(str(diccionario_funciones[funcion]['Parámetros']))),'|', diccionario_funciones[funcion]['Líneas'],' '*(longitud[1]-len(str(diccionario_funciones[funcion]['Líneas']))),'|', diccionario_funciones[funcion]['Invocaciones'],' '*(longitud[2]-len(str(diccionario_funciones[funcion]['Invocaciones']))),'|', diccionario_funciones[funcion]['Returns'],' '*(longitud[3]-len(str(diccionario_funciones[funcion]['Returns']))),'|', diccionario_funciones[funcion]['If'],' '*(longitud[4]-len(str(diccionario_funciones[funcion]['If']))),'|', diccionario_funciones[funcion]['For'],' '*(longitud[5]-len(str(diccionario_funciones[funcion]['For']))),'|', diccionario_funciones[funcion]['While'],' '*(longitud[6]-len(str(diccionario_funciones[funcion]['While']))),'|', diccionario_funciones[funcion]['Break'],' '*(longitud[7]-len(str(diccionario_funciones[funcion]['Break']))),'|', diccionario_funciones[funcion]['Exit'],' '*(longitud[8]-len(str(diccionario_funciones[funcion]['Exit']))),'|', diccionario_funciones[funcion]['Coment'],' '*(longitud[9]-len(str(diccionario_funciones[funcion]['Coment']))),'|', diccionario_funciones[funcion]['Ayuda'],' '*(longitud[10]-len(diccionario_funciones[funcion]['Ayuda'])),'|', diccionario_funciones[funcion]['Autor'], ' '*(m-len(diccionario_funciones[funcion]['Autor'])),'|')
 
         archivo.write('{},{},{},{},{},{},{},{},{},{},{},{},{}\n'.format( diccionario_funciones[funcion]['Funcion.Modulo'] , diccionario_funciones[funcion]['Parámetros'], diccionario_funciones[funcion]['Líneas'], diccionario_funciones[funcion]['Invocaciones'][0], diccionario_funciones[funcion]['Returns'], diccionario_funciones[funcion]['If'], diccionario_funciones[funcion]['For'], diccionario_funciones[funcion]['While'], diccionario_funciones[funcion]['Break'], diccionario_funciones[funcion]['Exit'], diccionario_funciones[funcion]['Coment'] , diccionario_funciones[funcion]['Ayuda'], diccionario_funciones[funcion]['Autor'] ))
 
