f209a55a8831b828ec558f96ffaeb1220e405be0
MateoVillarinos <65984178+MateoVillarinos@users.noreply.github.com>
2020-07-26 18:00:56 -0300

    Update arbol_de_invocacion.py

--- arbol_de_invocacion.py
+++ arbol_de_invocacion.py
@@ -1,18 +1,28 @@
 from consulta_de_funciones import listar_funciones
 
 def leer_archivo(archivo):
+    """[Autor: Mateo Villarinos]
+       [Ayuda: funcion que toma un modulo para devolver un diccionario con el nombre de las funciones
+       y como valor en la cantidad de lineas y las invocaciones]
+    """
     diccionario_de_funciones = listar_funciones()
-    contador1 = 0
-    contador2 = 1
+    lista_key = list(diccionario_de_funciones.keys())
     for linea in archivo:
         lista_linea = linea.rstrip("\n").split(",")
         diccionario_de_funciones[lista_linea[0]] = [str(len(lista_linea)-3)]
-        for campo in lista_linea[3:]:
-            if campo in diccionario_de_funciones and campo not in diccionario_de_funciones[lista_linea[0]]:
-                diccionario_de_funciones[lista_linea[0]].append(campo)
+        for titulo in lista_key:
+            i = 0
+            while i < len(lista_linea[3:]):
+                if titulo in lista_linea[3+i] and titulo not in diccionario_de_funciones[lista_linea[0]]:
+                    diccionario_de_funciones[lista_linea[0]].append(titulo)
+                else:
+                    i += 1
     return diccionario_de_funciones
 
 def buscar_main(archivo):
+    """[Autor: Mateo Villarinos]
+       [Ayuda: funcion que buscar cual es la funcion principal del programa]
+    """
     dicc_main = leer_archivo(archivo)
     i = 0
     main = ""
@@ -26,11 +36,14 @@ def buscar_main(archivo):
     return main
 
 def dibujar_arbol(archivo):
+    """[Autor: Mateo Villarinos]
+       [Ayuda: funcion que mediante de diferentes algoritmos va dibujando el arbon en una string]
+    """
     dicc_arbol = leer_archivo(archivo)
     principio = buscar_main(archivo)
     lista_de_seguimiento = []
     posiciones = [1]
-    flecha = "--->"
+    flecha = " ---> "
     indentacion = ""
     arbol = principio + "({})".format(dicc_arbol[principio][0])
     k, n = principio, 0
@@ -40,7 +53,7 @@ def dibujar_arbol(archivo):
             posiciones.append(1)
             lista_de_seguimiento.append(k)
             if arbol[-1:] == "\n":
-                indentacion = (n)*4*" "
+                indentacion = (n)*6*" "
                 for campo in lista_de_seguimiento:
                     indentacion += len(campo)*" "+(len(dicc_arbol[campo][0])+2)*" "
                 arbol += indentacion
@@ -62,6 +75,9 @@ def dibujar_arbol(archivo):
         
         
 def leer():
+    """[Autor: Mateo Villarinos]
+       [Ayuda: funcion main]
+    """
     fuente_unico= open('fuente_unico.csv','r' )
     print(dibujar_arbol(fuente_unico))
     fuente_unico.close()
