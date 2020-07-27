f47ca80276fed901f50976a92bfc98563ad313d2
Dany707 <67444608+Dany707@users.noreply.github.com>
2020-07-27 18:48:27 -0300

    Update analizador.py

--- analizador.py
+++ analizador.py
@@ -6,7 +6,8 @@ def diccionario_de_invocaciones(fuente_unico):
     n=1 #Variable que se utiliza para asignar un número a la funcion
     diccionario_de_invocaciones={}
     for linea in fuente_unico:
-        diccionario_de_invocaciones[linea[0]]={'Columna':n,'Invoca':[], 'Llamada':[] , 'Total Invocaciones':0}
+        lista=linea.rstrip('\n').split(',')
+        diccionario_de_invocaciones[lista[0]]={'Columna':n,'Invoca':[], 'Llamada':[] , 'Total Invocaciones':0}
         n+=1
     
     return diccionario_de_invocaciones
@@ -45,6 +46,7 @@ def analisis_codigo_funciones(fuente_unico):
 
     n=0 #Servirá como contador de funciones
     #Recorro línea por línea el csv
+    fuente_unico= open('fuente_unico.csv','r' )
     for linea in fuente_unico:
         #transfomo la línea a analizar en una lista
         lista=linea.rstrip('\n').split(',') 
@@ -61,29 +63,29 @@ def analisis_codigo_funciones(fuente_unico):
 
             if invocacion_a_funcion(funcion,lista)!=[]:
 
-                diccionario[funcion]['Total Invocaciones']+=invocacion_a_funcion(funcion,lista)[1]
-                #Suma el número de veces en que la Función_D es invocada por la Función_L
+                diccionario[invocacion_a_funcion(funcion,lista)[0]]['Total Invocaciones']+=invocacion_a_funcion(funcion,lista)[1]
+                #Cuenta el número de veces en que la Función_D es invocada por la Función_L
 
-                diccionario[lista[0]]['Invocada'].append(invocacion_a_funcion(funcion,lista))
-                #Escribe el nombre de la Función_D y el número de veces que es invocada por la Función_L
-
-                diccionario[invocacion_a_funcion(funcion,lista)[0]]['Llamada'].append(lista[0])
+                diccionario[funcion]['Invoca'].append(invocacion_a_funcion(funcion,lista))
                 #Escribe el nombre de la Función_L que invoca a la Función_D
 
+                diccionario[invocacion_a_funcion(funcion,lista)[0]]['Llamada'].append(funcion)
+                #Escribe el nombre de la Función_D que es invocada por la Función_L
 
     return [n,diccionario]
 
 
 def encabezado(n):
     """[Autor: Daniela Bolivar]
-       [Ayuda: Dado un número n se generará una lista donde la primera componente sea 'FUNCIONES' y el resto
+       [Ayuda: Dado un número n se quenerará una lista donde la primera componente sea 'FUNCIONES' y el resto
        los números del 1 al n]
     """
     encabezado=['FUNCIONES']
 
     for i in range(n):
-        encabezado.append(str(i+1))
+        encabezado.append(str(i+1)+' '*(2-len(str(i+1))))
 
+    
     return encabezado
 
 
@@ -96,36 +98,30 @@ def generacion_fila(n, funcion, diccionario):
     fila=[]
 
     for i in range(0,n+1): #Genero un a fila de n+1 componentes todas vacías
-        fila.append(' ')
+        fila.append('  ')
 
-    fila[0]=diccionario[funcion]['Columna'] + ' - '+ funcion #Cambio la primer componente por el número y nombre de la función
+    fila[0]=str(diccionario[funcion]['Columna']) + ' - '+ funcion #Cambio la primer componente por el número y nombre de la función
 
-    for j in range(0,len(diccionario[funcion]['Invoca']),2):
-        
-        k=diccionario[diccionario[funcion]['Invoca'][j]]['Columna']
+    for j in range(len(diccionario[funcion]['Invoca'])):
         #Obtengo el número del la función que es invocada por la fila
+        k=diccionario[diccionario[funcion]['Invoca'][j][0]]['Columna']
 
-        fila[k]=str(diccionario[funcion]['Invoca'][j+1])
-        #En la componente k+1 de la fila escribo el número de invocaciones
+        #En la fi
+        fila[k]=str(diccionario[funcion]['Invoca'][j][1])+' '*(2-len(str(diccionario[funcion]['Invoca'][j][1])))
 
     for l in range(len(diccionario[funcion]['Llamada'])):
-        
-        nombre=diccionario[funcion]['Llamada'][l]
-        #Del diccionario obtengo el nombre de la funcion que invoca a la función columna
-        m=diccionario[nombre]['Columna']
-        #Luego obtengo su número
 
-        fila[m]= 'X'
-        #En la componente m+1 de la fila escribo 'X'
+        m=diccionario[diccionario[funcion]['Llamada'][l]]['Columna']
 
+        fila[m]= 'X '
+
+   
     return fila
 
 
 def total_invocaciones( diccionario):
     """[Autor: Daniela Bolivar]
-       [Ayuda: Se generará una lista donde la primera componente sea 'Total Invocaciones' y el resto los números
-       correspondientes a las invocaciones de las funciones.
-       Si la función no tiene invocaciones se escribirá un ' ']
+       [Ayuda: Dada una función 'x' del programa, se analiza si ésta invoca a otra función 'y']
     """
 
     total=['Total Invocaciones']
@@ -134,26 +130,27 @@ def total_invocaciones( diccionario):
 
         if diccionario[funcion]['Total Invocaciones'] !=0:
 
-            total.append(str(diccionario[funcion]['Total Invocaciones']))
+            total.append(str(diccionario[funcion]['Total Invocaciones'])+ ' '*(2-len(str(diccionario[funcion]['Total Invocaciones']))))
 
         else:
-            total.append(' ')
+            total.append('  ')
 
+    
     return total
 
 
 def longitud_primera_columna(diccionario):
     """[Autor: Daniela Bolivar]
-       [Ayuda: Dada la frase 'Total de Invocaciones' comparo su longitud con la correspondiente a el número de la
-        función más los nombres de las funciones del archivo.
-       La función devuelve el máximo número que se obtiene de estas comparaciones]
+       [Ayuda: Danda una palabra, comparo su longitud con la correspondiente a los elementos que se obtienen
+       del diccionario en la clave  ingresados.
+       La función devuelve el máximo número que se obtiene de estas comparaciones más dos]
     """
     n=len('Total Invocaciones')
 
     for funcion in diccionario:
 
-        if n<len(diccionario[funcion]['Columna'] + ' - '+ funcion):
-            n=len(diccionario[funcion]['Columna'] + ' - '+ funcion)
+        if n<len(str(diccionario[funcion]['Columna']) + ' - '+ funcion):
+            n=len(str(diccionario[funcion]['Columna']) + ' - '+ funcion)
 
     return n
 
@@ -161,15 +158,17 @@ def longitud_primera_columna(diccionario):
 
 def creacion_tabla(fuente_unico):
     """[Autor: Daniela Bolivar]
-       [Ayuda: Dado un archivo creo una lista de listas con la información analizada por las anteriores funciones]
+       [Ayuda: Dada una función 'x' del programa, se analiza si ésta invoca a otra función 'y']
     """
-
+    fuente_unico= open('fuente_unico.csv','r' )
     diccionario_funciones=analisis_codigo_funciones(fuente_unico)
 
     k=longitud_primera_columna(diccionario_funciones[1])
 
-    tabla=encabezado(diccionario_funciones[0])
-    tabla[0]=tabla[0]+ ' '*(k+3-len(tabla[0]))
+    
+    tabla=[encabezado(diccionario_funciones[0])]
+    
+    tabla[0][0]=tabla[0][0]+ ' '*(k+3-len(tabla[0][0]))
 
     for funcion in diccionario_funciones[1]:
         lista=generacion_fila(diccionario_funciones[0], funcion, diccionario_funciones[1])
@@ -180,24 +179,26 @@ def creacion_tabla(fuente_unico):
     totales[0]=totales[0]+ ' '*(k+3-len(totales[0]))
     tabla.append(totales)
 
+    
+
             
     return tabla
 
 
 def imprimir(archivo,fuente_unico):
     """[Autor: Daniela Bolivar]
-       [Ayuda: Dado un archivo se escribre sobre este una tabla. La cual también se imprime por pantalla]
+       [Ayuda: Dada una función 'x' del programa, se analiza si ésta invoca a otra función 'y']
     """
     tabla= creacion_tabla(fuente_unico)
 
     for lista in tabla:
         fila=' | '.join(lista)
 
-        print(fila, '|')
-        archivo.write(fila, '|')
+        print(fila + '|')
+        archivo.write(fila + '|')
 
         print('-'*(len(fila)+2))
-        archivo.write(fila, '|')
+        archivo.write(fila + '|')
     
     return
 
@@ -205,7 +206,7 @@ def imprimir(archivo,fuente_unico):
 
 def generar_analizador():
     """[Autor: Daniela Bolivar]
-       [Ayuda: Esta función abre los archivos correspondientes, crea el archivo pedido e imprime la información]
+       [Ayuda: Dada una función 'x' del programa, se analiza si ésta invoca a otra función 'y']
     """
 
     fuente_unico= open('fuente_unico.csv','r' )
